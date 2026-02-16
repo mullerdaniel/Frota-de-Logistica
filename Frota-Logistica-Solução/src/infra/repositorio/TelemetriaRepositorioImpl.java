0b11e62edc3aac67b988aed5543aeca50ea17fe1
eduarda_stein <eduarda_stein@estudante.sesisenai.org.br>
2026-02-16 19:32:20 -0300

    FEAT: criar implementação do repositório para salvar telemetria

--- Frota-Logistica-Solução/src/infra/repositorio/TelemetriaRepositorioImpl.java
+++ Frota-Logistica-Solução/src/infra/repositorio/TelemetriaRepositorioImpl.java
@@ -1,7 +1,15 @@
 package infra.repositorio;
 
+import domain.entidade.Telemetria;
+import domain.repositorio.TelemetriaRepositorio;
 
+public class TelemetriaRepositorioImpl implements TelemetriaRepositorio {
 
-public class TelemetriaRepositorioImpl {
+    @Override
+    public void salvar(Telemetria t) {
 
+        System.out.println("Simulando salvamento da telemetria...");
+        System.out.println("Velocidade: " + t.velocidade);
+
+    }
 }
