be23c3293b505b30f25815bf6508997327167dd2
eduarda_stein <eduarda_stein@estudante.sesisenai.org.br>
2026-02-16 19:18:26 -0300

    FEAT: implementar processamento de telemetria via fila em alta carga

--- Frota-Logistica-Solução/src/domain/estrategia/EstrategiaAltaCarga.java
+++ Frota-Logistica-Solução/src/domain/estrategia/EstrategiaAltaCarga.java
@@ -1,6 +1,18 @@
 package domain.estrategia;
 
+import domain.entidade.Telemetria;
+import infra.mensagem.FilaMensagens;
 
-public class EstrategiaAltaCarga {
+public class EstrategiaAltaCarga implements EstrategiaIngestaoDados {
 
+    private FilaMensagens fila;
+
+    public EstrategiaAltaCarga(FilaMensagens fila) {
+        this.fila = fila;
+    }
+
+    public void processar(Telemetria t) {
+        System.out.println("Alta carga - enviando para fila");
+        fila.enviar(t);
+    }
 }
