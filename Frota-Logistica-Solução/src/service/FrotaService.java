a91c2e7c76e461640bf11af81b993a12b98ba228
Daniel Müller <miladaniel370@gmail.com>
2026-02-16 19:42:26 -0300

    FEAT: Implementa estratégia de ingestão de dados na FrotaService

--- Frota-Logistica-Solução/src/service/FrotaService.java
+++ Frota-Logistica-Solução/src/service/FrotaService.java
@@ -1,6 +1,17 @@
 package service;
 
+import domain.entidade.Telemetria;
+import domain.estrategia.EstrategiaIngestaoDados;
+
 public class FrotaService {
 
+    private EstrategiaIngestaoDados estrategia;
 
+    public FrotaService(EstrategiaIngestaoDados estrategia) {
+        this.estrategia = estrategia;
+    }
+
+    public void receber(Telemetria t) {
+        estrategia.processar(t);
+    }
 }
