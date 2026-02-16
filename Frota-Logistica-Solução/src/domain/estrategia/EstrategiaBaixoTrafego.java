aaca799ba0448052afe9b36f584e0dcb3af514f8
eduarda_stein <eduarda_stein@estudante.sesisenai.org.br>
2026-02-16 18:42:55 -0300

    FEAT: implementar estratégia de ingestão para baixo tráfego

--- Frota-Logistica-Solução/src/domain/estrategia/EstrategiaBaixoTrafego.java
+++ Frota-Logistica-Solução/src/domain/estrategia/EstrategiaBaixoTrafego.java
@@ -1,5 +1,18 @@
 package domain.estrategia;
 
-public class EstrategiaBaixoTrafego {
+import domain.entidade.Telemetria;
+import domain.repositorio.TelemetriaRepositorio;
 
+public class EstrategiaBaixoTrafego implements EstrategiaIngestaoDados {
+
+    private TelemetriaRepositorio repositorio;
+
+    public EstrategiaBaixoTrafego(TelemetriaRepositorio repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public void processar(Telemetria t) {
+        System.out.println("Baixo trafego - salvando direto");
+        repositorio.salvar(t);
+    }
 }
