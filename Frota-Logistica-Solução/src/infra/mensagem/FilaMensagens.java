30e72d6c44b987ee06f9c1e421af08ea13f55f91
Daniel Müller <miladaniel370@gmail.com>
2026-02-16 18:55:42 -0300

    FEAT: Criando metodo enviar

--- Frota-Logistica-Solução/src/infra/mensagem/FilaMensagens.java
+++ Frota-Logistica-Solução/src/infra/mensagem/FilaMensagens.java
@@ -1,5 +1,13 @@
 package infra.mensagem;
 
+import domain.entidade.Telemetria;
+
 public class FilaMensagens {
 
+    public void enviar(Telemetria t) {
+
+        System.out.println("Enviando telemetria para fila...");
+        System.out.println("Velocidade: " + t.velocidade);
+
+    }
 }
